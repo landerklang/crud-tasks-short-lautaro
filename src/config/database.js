e0e180c163561174d827358d0c91b9e356f312d8
¨lautaro <shortlauty35@gmail.com>
2025-08-06 19:31:08 -0300

    se añadio la conexion a la base de datos

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+import dotenv from dotenv;
+dotenv.config();
+
+export const sequelize=new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DIALECT,
+    }
+);
+
+export const starOn=async()=>{
+    try {
+        await sequelize.sync();
+        console.log("conexion exitosa")
+    } catch (error) {
+        console.error("no se pudo conectar la base de datos",error);
+    }
+};
